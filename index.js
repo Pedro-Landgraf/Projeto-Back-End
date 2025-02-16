1309a1399e51d68d048520f4ccd767adcff86142
Pedro-Landgraf <143462985+Pedro-Landgraf@users.noreply.github.com>
2025-02-16 14:32:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,11 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
-// Configuração do mecanismo de visualização Mustache
 var engine = mustacheExpress();
 app.engine("mustache", engine);
 app.set('views', path.join(__dirname, 'Views'));
 app.set('view engine', 'mustache');
 
-// Middlewares
 app.use(express.json());
 app.use(cookieParser());
 connectDB();
