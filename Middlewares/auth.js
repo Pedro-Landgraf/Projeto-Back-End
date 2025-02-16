d18000d14baefc0c8296aa25987212d03c6a2f52
Pedro-Landgraf <143462985+Pedro-Landgraf@users.noreply.github.com>
2025-02-16 14:32:39 -0300

    Update auth.js

--- Middlewares/auth.js
+++ Middlewares/auth.js
@@ -1,48 +1,36 @@
 const jwt = require('jsonwebtoken');
-const { users } = require('../Data/databaseUsers');
 
 const verifyToken = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
+    const token = req.cookies.token;
     if (!token) {
-        return res.status(401).json({ message: 'Acesso negado' });
+        return res.status(403).json({ message: 'Token não fornecido' });
     }
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(403).json({ message: 'Token inválido!' });
-        }
-        const user = users.find(u => u.id === decoded.id);
-        if (!user) {
-            return res.status(401).json({ message: 'Usuário não encontrado' });
-        }
-        req.user = user;
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
         next();
-    });
+    } catch (err) {
+        res.status(401).json({ message: 'Token inválido' });
+    }
 };
 
 const isAdm = (req, res, next) => {
-    verifyToken(req, res, () => {
-        if (!req.user || !req.user.isAdmin) {
-            return res.status(403).json({ message: 'Acesso negado: apenas administradores podem realizar a ação' });
-        }
-        next();
-    });
+    if (!req.user.isAdmin) {
+        return res.status(403).json({ message: 'Permissão negada' });
+    }
+    next();
 };
 
 const userIsAdmOrHimself = (req, res, next) => {
-    verifyToken(req, res, () => {
-        const idUserToBeUpdated = parseInt(req.params.id, 10); // Supondo que o ID seja passado como parâmetro na URL
-        const idUserMakingUpdate = req.user.id;
-        const isAdm = req.user.isAdmin;
-        if (!isAdm && idUserToBeUpdated !== idUserMakingUpdate) {
-            return res.status(403).json({ message: 'Acesso negado: apenas administradores podem realizar a ação' });
-        }
+    if (req.user.isAdmin || req.user.id === parseInt(req.params.id)) {
         next();
-    });
+    } else {
+        res.status(403).json({ message: 'Permissão negada' });
+    }
 };
 
-const urlNotValid = (req, res, next) => {
-    res.status(404).json({ message: 'Rota não existente' });
+module.exports = {
+    verifyToken,
+    isAdm,
+    userIsAdmOrHimself
 };
-
-module.exports = { verifyToken, isAdm, userIsAdmOrHimself, urlNotValid };
