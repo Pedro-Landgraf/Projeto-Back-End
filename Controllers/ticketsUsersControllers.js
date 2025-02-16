4852688c6e6a2dc62521aaafd6d3ac75ffb3ce67
Pedro-Landgraf <143462985+Pedro-Landgraf@users.noreply.github.com>
2025-02-16 12:00:29 -0300

    Update ticketsUsersControllers.js

--- Controllers/ticketsUsersControllers.js
+++ Controllers/ticketsUsersControllers.js
@@ -1,14 +1,17 @@
 const { ticketsUsers } = require('../Data/databaseTicketsUsers.js')
 const connectDB = require('../Middlewares/db.js')
 const TicketUsers = require('../Models/Tickets-Users.js')
+const Users = require('../Models/Users.js')
 
 connectDB()
 
-const buyTickets = async (name, idUser, total) => {
+const buyTickets = async (req, res) => {
+    const ticketName = req.body
+
     try {
-        let ticket = await TicketUsers.find({name: name, idUser: idUser});
+        let ticket = await TicketUsers.find({name: ticketName.name, idUser: ticketName.idUser});
         if(!ticket){
-            await TicketUsers.insertOne ({name: name, idUser: idUser, total: total})
+            await TicketUsers.insertOne ({name: ticket.name, idUser: ticket.idUser, total: ticket.total})
         }
         else{
             ticket.total += total
@@ -21,8 +24,9 @@ const buyTickets = async (name, idUser, total) => {
     }
 }
 
-const seeMyTickets = async (idUser) => {
+const seeMyTickets = async (res) => {
     try {
+        
         const tickets = await TicketUsers.find({idUser: idUser});
         res.json(tickets);
     } catch (err) {
