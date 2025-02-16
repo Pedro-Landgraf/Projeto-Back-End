0ff7a008dc0993d7ec6113420a86e83755d08240
Pedro-Landgraf <143462985+Pedro-Landgraf@users.noreply.github.com>
2025-02-16 14:23:19 -0300

    Update databaseTicketsUsers.js

--- Data/databaseTicketsUsers.js
+++ Data/databaseTicketsUsers.js
@@ -1,21 +1,21 @@
-const TicketUser = require('../Models/Tickets-Users');
-const connectDB = require('../Middlewares/db.js')
-
-connectDB()
+const TicketUser = require('../Models/TicketUser');
+const connectDB = require('../Middlewares/db.js');
 
 const insertTicketsUsers = async () => {
-    const ticketsUsers = [
-        {name: 'Ingresso V.I.P.', id: 1, total: 2},
-        {name: 'Ingresso Camarote', id: 1, total: 1},
-        {name: 'Ingresso V.I.P.', id: 2, total: 5},
-    ]
-
     try {
+        await connectDB();
+        const ticketsUsers = [
+            { name: 'Ingresso V.I.P.', userId: 1, total: 2 },
+            { name: 'Ingresso Camarote', userId: 1, total: 1 },
+            { name: 'Ingresso V.I.P.', userId: 2, total: 5 }
+        ];
         const result = await TicketUser.insertMany(ticketsUsers);
         console.log('Tickets inserted:', result);
     } catch (err) {
         console.error('Error inserting tickets:', err);
+    } finally {
+        process.exit(0);
     }
 };
 
-insertTicketsUsers()
+insertTicketsUsers();
