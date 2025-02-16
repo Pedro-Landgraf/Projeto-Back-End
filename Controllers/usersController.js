66c986ed92f6924a1066ad3c06e4f90732dd8d52
Pedro-Landgraf <143462985+Pedro-Landgraf@users.noreply.github.com>
2025-02-16 14:21:20 -0300

    Update usersController.js

--- Controllers/usersController.js
+++ Controllers/usersController.js
@@ -1,68 +1,64 @@
-const { users, getNewId } = require('../Data/databaseUsers.js')
-const jwt = require('jsonwebtoken')
-const connectDB = require('../Middlewares/db.js')
-const User = require('../Models/Users.js')
-
-connectDB()
+const User = require('../Models/Users.js');
+const jwt = require('jsonwebtoken');
 
 const createUser = async (req, res) => {
-    const newUser =  req.body
+    const newUser = req.body;
     if (!newUser.name || !newUser.email || !newUser.password) {
         return res.status(400).json({ message: 'Todos os campos são obrigatórios' });
     }
-
-    else{
-        try{
-        const userCreated = await User.insertOne ({id: getNewId(), name: newUser.name, email: newUser.email, password: newUser.password, isAdmin: false})
-        userCreated.save()
-        res.status(200).json(userCreated)
-        }
-        catch(err){
-            res.status(500).json({ message: 'Erro' });
-        }
+    try {
+        const userCreated = await User.create({
+            name: newUser.name,
+            email: newUser.email,
+            password: newUser.password,
+            isAdmin: false
+        });
+        res.status(200).json(userCreated);
+    } catch (err) {
+        res.status(500).json({ message: 'Erro ao criar usuário', error: err.message });
     }
-}
-
+};
 
 const createUserAdm = async (req, res) => {
-    
-    const newUser =  req.body
-
+    const newUser = req.body;
     if (!newUser.name || !newUser.email || !newUser.password) {
         return res.status(400).json({ message: 'Todos os campos são obrigatórios' });
     }
+    try {
+        const userCreated = await User.create({
+            name: newUser.name,
+            email: newUser.email,
+            password: newUser.password,
+            isAdmin: true
+        });
+        res.status(200).json(userCreated);
+    } catch (err) {
+        res.status(500).json({ message: 'Erro ao criar administrador', error: err.message });
+    }
+};
 
-    else{
-        try{
-        const userCreated = await User.insertOne ({id: getNewId(), name: newUser.name, email: newUser.email, password: newUser.password, isAdmin: false})
-        userCreated.save()
-        res.status(200).json(userCreated)
+const verifyUser = async (req, res) => {
+    res.render('login', { erro: '' });
+};
+
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const userLogin = await User.findOne({ email, password });
+        if (!userLogin) {
+            return res.render('login', { erro: 'Usuário ou senha inválida, tente novamente' });
         }
-        catch(err){
-            res.status(500).json({ message: 'Erro' });
-        }
+        const token = jwt.sign({ id: userLogin._id, isAdmin: userLogin.isAdmin }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.cookie("token", token, { httpOnly: true });
+        res.redirect('/getTickets');
+    } catch (err) {
+        res.render('login', { erro: 'Erro ao verificar usuário' });
     }
-}
-
-const verifyUser = async(req, res) => {
-    const {email, password} = req.body
-
-    const userLogin = User.find({email: email, password: password})
-
-    if(!userLogin) {
-        return res.status(401).json({ message: 'Usuário ou senha inválida, tente novamente' });
-    }
-
-    else{
-        const token = jwt.sign({id: userLogin.id, isAdm: userLogin.isAdm}, process.env.JWT_SECRET, {expiresIn: '1 hr'})
-        res.cookie("token", token, {httpOnly: true})
-
-        res.status(200).json({message: 'Login funcionou', token})
-    }
-}
+};
 
 module.exports = {
     createUser,
     createUserAdm,
     verifyUser,
-}
+    loginUser
+};
