16528ff499bf99c76c6497dffdd581af9bc14a5b
Pedro-Landgraf <143462985+Pedro-Landgraf@users.noreply.github.com>
2025-02-16 14:33:01 -0300

    Update db.js

--- Middlewares/db.js
+++ Middlewares/db.js
@@ -1,17 +1,16 @@
 const mongoose = require('mongoose');
 
-// MongoDB connection URI
-const uri = 'mongodb://localhost:27017';
-
-// Function to connect to the database
 const connectDB = async () => {
-  try {
-    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    console.log('Connected to MongoDB');
-  } catch (err) {
-    console.error('Error connecting to MongoDB:', err);
-    process.exit(1); // Exit process with failure
-  }
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('MongoDB connected...');
+    } catch (err) {
+        console.error('MongoDB connection error:', err.message);
+        process.exit(1);
+    }
 };
 
 module.exports = connectDB;
