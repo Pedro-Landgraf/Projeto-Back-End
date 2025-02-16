e27a4a92f8207e11e43459d81bc738e2ddc94460
Pedro-Landgraf <143462985+Pedro-Landgraf@users.noreply.github.com>
2025-02-16 14:20:22 -0300

    Update databaseUsers.js

--- Data/databaseUsers.js
+++ Data/databaseUsers.js
@@ -1,23 +1,34 @@
+const connectDB = require('../Middlewares/db.js');
 const User = require('../Models/Users.js');
-const connectDB = require('../Middlewares/db.js')
 
-connectDB()
+let currentId = 0;
+
+const getNewId = () => {
+    currentId += 1;
+    return currentId;
+};
 
 const insertUsers = async () => {
-    const users = [
-        {id: 1, name: 'Pedro', email: 'pedrolandgraf@alunos.utfpr.edu.br', password: '12345', isAdmin: true},
-        {id: 2, name: 'Joao', email: 'pedrolandgraf@alunos.utfpr.edu.br', password: '54321', isAdmin: false},
-        {id: 3, name: 'Jose', email: 'pedrolandgraf@alunos.utfpr.edu.br', password: 'Ze222', isAdmin: false},
-        {id: 4, name: 'Maria', email: 'pedrolandgraf@alunos.utfpr.edu.br', password: 'MS@ll', isAdmin: true},
-        {id: 5, name: 'Marcos', email: 'pedrolandgraf@alunos.utfpr.edu.br', password: '00000', isAdmin: false}
-    ]
-
     try {
+        await connectDB();
+        const users = [
+            { id: getNewId(), name: 'Pedro', email: 'pedrolandgraf@alunos.utfpr.edu.br', password: '12345', isAdmin: true },
+            { id: getNewId(), name: 'Joao', email: 'joao@alunos.utfpr.edu.br', password: '54321', isAdmin: false },
+            { id: getNewId(), name: 'Jose', email: 'jose@alunos.utfpr.edu.br', password: 'Ze222', isAdmin: false },
+            { id: getNewId(), name: 'Maria', email: 'maria@alunos.utfpr.edu.br', password: 'MS@ll', isAdmin: true },
+            { id: getNewId(), name: 'Marcos', email: 'marcos@alunos.utfpr.edu.br', password: '00000', isAdmin: false }
+        ];
         const result = await User.insertMany(users);
         console.log('Users inserted:', result);
     } catch (err) {
         console.error('Error inserting users:', err);
+    } finally {
+        process.exit(0);
     }
 };
 
 insertUsers();
+
+module.exports = {
+    getNewId
+};
