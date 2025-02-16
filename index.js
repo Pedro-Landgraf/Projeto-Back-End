b57fa31015e1c12ef36dbb70c7bf590e72d5154e
Pedro-Landgraf <143462985+Pedro-Landgraf@users.noreply.github.com>
2025-02-16 13:22:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//Importação de frameworks e funções no projeto, uso de dotenv para o auth.
+
 require('dotenv').config();
 const express = require('express')
 const connectDB = require('./Middlewares/db.js')
@@ -18,11 +18,8 @@ app.use(express.json())
 
 connectDB()
 
-//O servidor rodará na porta 4000, tendo 3 rotas principais, /users, /countries, /cities.
 app.use('/', routes)
 
-
-//Chamada de middleware do arquivo auth.js para caso não encontre a rota
 app.use(urlNotValid)
 
 app.listen(4000, () => console.log("Servidor rodando na porta 4000"))
