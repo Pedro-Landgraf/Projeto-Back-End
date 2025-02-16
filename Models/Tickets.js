f672899bfadb96b0ff2731ea535bf7a1e366f50f
Pedro-Landgraf <143462985+Pedro-Landgraf@users.noreply.github.com>
2025-02-16 14:21:42 -0300

    Update Tickets.js

--- Models/Tickets.js
+++ Models/Tickets.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const ticketSchema = new mongoose.Schema({
-    name: String,
-    price: Number,
-    total: Number
-})
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    total: { type: Number, required: true }
+});
 
 module.exports = mongoose.model('Ticket', ticketSchema);
